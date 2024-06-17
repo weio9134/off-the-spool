a2022cd1a266867c180adfa7f6587445554570ad
Wei Ouyang <wo52@cornell.edu>
2024-06-17 16:02:39 -0400

    Added clerk to authenticate login - implemented sign and sign up page with clerk component - integrated middleware.ts to ignore public routes and protect all else

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { ReactNode } from "react";
+import { Inter } from "next/font/google";
+import "../globals.css"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Off the Spool",
+  description: "A Threads Clone",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
