f63c9b6fd9da4d68a321629fa8cd79665fb1c529
Wei Ouyang <wo52@cornell.edu>
2024-06-18 20:45:56 -0400

    Deployed site for web endpoint - obtained endpoint from clerk to manage communities - allowed user posts to be associated with communities - updated thread actions for communities

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -18,16 +18,18 @@ import { Textarea } from '../ui/textarea'
 import { usePathname, useRouter } from 'next/navigation'
 import { ThreadValidation } from '@/lib/validations/thread'
 import { createThread } from '@/lib/actions/thread.actions'
+import { useOrganization } from '@clerk/nextjs'
 
 const PostThread = ({ userId }: { userId: string }) => {
   const router = useRouter()
   const pathname = usePathname()
+  const { organization } = useOrganization()
 
   const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     })
 
